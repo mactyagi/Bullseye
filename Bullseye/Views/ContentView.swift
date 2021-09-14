89f72d156e809da13b8447cb335583352ffacacc
manukant tyagi <mactyagi@icloud.com>
2021-09-14 07:29:00 +0530

    Extract the Views

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -12,46 +12,81 @@ struct ContentView: View {
     @State private var sliderValue = 10.0
     @State private var game = Game()
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE AS YOU CAN DO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .font(.footnote)
-                .lineSpacing(4.0)
-                .padding()
-            Text(String(game.target))
-                .bold()
-                .kerning(-1.0)
-                .fontWeight(.black)
-                .font(.largeTitle)
-            HStack {
-                Text("1")
-                    .bold()
-                
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                InstructionView(game: $game)
+                sliderView(sliderValue: $sliderValue)
+                HitMeButtonView(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
             }
-            
-
-            Button(action: {
-                print("Hello SwiftUI!")
-                alertIsVisible = true
-            }) {
-                Text("Hit me")
-            }
-            .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue = Int(sliderValue.rounded())
-                return Alert(title: Text("Hello there"), message: Text("The slider value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) point in this round."), dismissButton: .default(Text("Awesome")))
-            })
         }
+    }
+}
+
+struct InstructionView: View{
+    @Binding var game: Game
+    var body: some View{
+        VStack{
+            InstructionText(text: "🎯🎯🎯🎯\n Put the bullseye as close as you can to")
+                .padding(.leading, 30)
+                .padding(.trailing, 30)
+            BigNumberText(text: String(game.target))
+        }
+    }
+}
+
+
+struct sliderView: View{
+    @Binding var sliderValue: Double
+    var body: some View{
+        HStack {
+            SliderLabelText(text: "1")
+            
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            SliderLabelText(text: "100")
+        }
+        .padding()
+    }
+}
+
+struct HitMeButtonView: View {
+    @Binding var alertIsVisible : Bool
+    @Binding var sliderValue : Double
+    @Binding var game : Game
+    var body: some View{
+        Button(action: {
+            print("Hello SwiftUI!")
+            alertIsVisible = true
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+            
+        }
+        .padding(20.0)
+        .background(ZStack {
+            Color("ButtonColor")
+            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+        })
+        
+        .foregroundColor(.white)
+        .cornerRadius(21)
+        
+         
+        .alert(isPresented: $alertIsVisible, content: {
+            let roundedValue = Int(sliderValue.rounded())
+            return Alert(title: Text("Hello there"), message: Text("The slider value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) point in this round."), dismissButton: .default(Text("Awesome")))
+        })
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
 //        ContentView()
 //        .previewLayout(.fixed(width: 568, height: 320))
     }
---
+++ Bullseye/Views/TextViews.swift
@@ -0,0 +1,57 @@
+//
+//  SwiftUIView.swift
+//  Bullseye
+//
+//  Created by manukant tyagi on 13/09/21.
+//
+
+import SwiftUI
+
+struct InstructionText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text.uppercased())
+            .bold()
+            .kerning(2.0)
+            .multilineTextAlignment(.center)
+            .font(.footnote)
+            .lineSpacing(4.0)
+            
+            .foregroundColor(Color("TextColor"))
+    }
+}
+
+struct BigNumberText: View {
+    var text: String
+    var body: some View{
+        
+        Text(text)
+            .bold()
+            .foregroundColor(Color("TextColor"))
+            .kerning(-1.0)
+            .fontWeight(.black)
+            .font(.largeTitle)
+    }
+}
+
+struct SliderLabelText: View {
+    var text: String
+    var body: some View{
+        Text(text)
+            .bold()
+            .foregroundColor(Color("TextColor"))
+        
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            InstructionText(text: "🎯🎯🎯🎯\n Put the bullseye as close as you can to")
+            BigNumberText(text: "999")
+            SliderLabelText(text: "99")
+        }
+        
+    }
+}
